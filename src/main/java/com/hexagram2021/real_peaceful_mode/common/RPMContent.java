2fc5bf85d5edd4ee35ecfd28c579218f017ed4be
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-06-27 19:43:52 +0800

    更新贴图和分工

--- src/main/java/com/hexagram2021/real_peaceful_mode/common/RPMContent.java
+++ src/main/java/com/hexagram2021/real_peaceful_mode/common/RPMContent.java
@@ -1,26 +1,21 @@
 package com.hexagram2021.real_peaceful_mode.common;
 
-import com.hexagram2021.emeraldcraft.common.register.ECConfiguredStructures;
-import com.hexagram2021.emeraldcraft.common.register.ECStructureSets;
-import com.hexagram2021.emeraldcraft.common.register.ECStructures;
 import com.hexagram2021.real_peaceful_mode.common.crafting.compat.ModsCompatManager;
 import com.hexagram2021.real_peaceful_mode.common.register.*;
-import net.minecraft.world.level.levelgen.feature.StructureFeature;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegisterEvent;
 
 import java.util.function.Consumer;
 
-import static com.hexagram2021.emeraldcraft.EmeraldCraft.MODID;
+import static com.hexagram2021.real_peaceful_mode.RealPeacefulMode.MODID;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RPMContent {
 	public static void modConstruction(IEventBus bus, Consumer<Runnable> runLater) {
 		ModsCompatManager.compatModLoaded();
 
-		//RPMWoodType.init();
 		RPMBlocks.init(bus);
 		RPMItems.init(bus);
 	}
@@ -30,9 +25,7 @@ public class RPMContent {
 	}
 
 	@SubscribeEvent
-	public static void registerStructures(RegistryEvent.Register<StructureFeature<?>> event) {
-		ECStructures.init(event);
-		ECConfiguredStructures.init();
-		ECStructureSets.init();
+	public static void onRegister(RegisterEvent event) {
+
 	}
 }
