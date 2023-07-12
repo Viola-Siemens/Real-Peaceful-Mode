5957fe101cb334dc0f54abe0e81e151433a0e40a
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-07-12 21:58:55 +0800

    解决任务NPC僵尸攻击玩家的问题

--- src/main/java/com/hexagram2021/real_peaceful_mode/mixin/LivingEntityMixin.java
+++ src/main/java/com/hexagram2021/real_peaceful_mode/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package com.hexagram2021.real_peaceful_mode.mixin;
 
 import com.hexagram2021.real_peaceful_mode.common.crafting.menu.MissionMessageMenu;
+import com.hexagram2021.real_peaceful_mode.common.entity.IFriendlyMonster;
 import com.hexagram2021.real_peaceful_mode.common.entity.IMonsterHero;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,6 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class LivingEntityMixin {
 	@Inject(method = "canAttack(Lnet/minecraft/world/entity/LivingEntity;)Z", at = @At(value = "HEAD"), cancellable = true)
 	public void ignoreHeroOrPlayerWithMenu(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir) {
+		if(this instanceof IFriendlyMonster friendlyMonster) {
+			if(friendlyMonster.preventAttack(livingEntity)) {
+				cir.setReturnValue(false);
+				cir.cancel();
+			}
+		}
 		if(livingEntity instanceof ServerPlayer player) {
 			if(((IMonsterHero)player).isHero(((LivingEntity)(Object)this).getType())) {
 				cir.setReturnValue(false);
