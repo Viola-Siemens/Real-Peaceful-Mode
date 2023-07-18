67de05cffb38d3f054856c503f7841910d0b3be1
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-07-18 10:39:25 +0800

    修复魔法池小屋生成问题

--- src/main/java/com/hexagram2021/real_peaceful_mode/common/world/structures/AbandonedMagicPoolFeature.java
+++ src/main/java/com/hexagram2021/real_peaceful_mode/common/world/structures/AbandonedMagicPoolFeature.java
@@ -5,6 +5,7 @@ import com.hexagram2021.real_peaceful_mode.common.world.structures.pieces.Abando
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
@@ -24,7 +25,14 @@ public class AbandonedMagicPoolFeature extends Structure {
 	}
 
 	private static void generatePieces(StructurePiecesBuilder builder, GenerationContext context) {
-		BlockPos blockpos = new BlockPos(context.chunkPos().getMinBlockX(), 65, context.chunkPos().getMinBlockZ());
+		BlockPos centerOfChunk = new BlockPos(context.chunkPos().getMinBlockX() + 7, 0, context.chunkPos().getMinBlockZ() + 7);
+		BlockPos blockpos = new BlockPos(
+				centerOfChunk.getX(),
+				context.chunkGenerator().getBaseHeight(
+						centerOfChunk.getX(), centerOfChunk.getZ(), Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor(), context.randomState()
+				),
+				centerOfChunk.getZ()
+		);
 		Rotation rotation = Rotation.getRandom(context.random());
 		AbandonedMagicPoolPieces.addPieces(context.structureTemplateManager(), blockpos, rotation, builder);
 	}
