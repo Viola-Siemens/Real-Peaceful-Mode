1a45d4ec1f551b8e63eb7574aecb48bf831c1d2c
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-09-26 13:27:39 +0800

    碎石地物不能出现在非完整方块上，并削弱其生成数量

--- src/main/java/com/hexagram2021/real_peaceful_mode/common/block/ChippingsBlock.java
+++ src/main/java/com/hexagram2021/real_peaceful_mode/common/block/ChippingsBlock.java
@@ -1,7 +1,9 @@
 package com.hexagram2021.real_peaceful_mode.common.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -16,5 +18,10 @@ public class ChippingsBlock extends Block {
 	@Override
 	public VoxelShape getShape(BlockState blockState, BlockGetter level, BlockPos pos, CollisionContext context) {
 		return Block.box(4.0D, 0.0D, 4.0D, 12.0D, 1.0D, 12.0D);
+	}
+
+	@Override
+	public boolean canSurvive(BlockState blockState, LevelReader level, BlockPos pos) {
+		return isFaceFull(level.getBlockState(pos.below()).getCollisionShape(level, pos.below()), Direction.UP);
 	}
 }
